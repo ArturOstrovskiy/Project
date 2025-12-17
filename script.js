c7a2118526ac3ee2771664063fc4931b6984d281
Artur Ostrovskiy <artur_ostrovskiy@mail.ru>
2025-12-17 18:25:32 +0300

    18:25 17.12

---
+++ script.js
@@ -0,0 +1,69 @@
+const model = {
+	//Массив с заметками
+	notes: [],
+	addNotes(name, value){
+		console.log(name, value)
+		const id = Math.random()
+		const newNote = { id, name, value }
+		this.notes.push(newNote)
+		console.log(this.notes.length)
+		view.renderNotes(model.notes);
+	}
+
+}
+
+
+const view = {
+	init() {
+		this.renderNotes(model.notes);
+
+		const inputName = document.querySelector('#name-notes')
+		const inputValue = document.querySelector('#value-notes')
+		const addButton = document.querySelector('.add-button')
+
+		//Передаем по клику на кнопку содержимое обоих инпутов
+		addButton.addEventListener('click', () => {
+			const name = inputName.value
+			const value = inputValue.value
+			controller.addNotes(name,value)
+			inputName.value = ''
+			inputValue.value = ''
+		})
+	},
+	renderNotes(notes) {
+		const notesBlock = document.querySelector('.notes-block')
+		let notesHTML = ''
+
+		for (const note of notes) {
+			notesHTML += `
+        <li id="${note.id}" class="movie">
+          <b class="note-title">${note.name}</b>
+          <p class="note-description">${note.value}</p>
+          <button class="delete-button" type="button">Удалить 🗑</button>
+        </li>
+      `
+		}
+
+		notesBlock.innerHTML = notesHTML
+	},
+
+
+}
+
+const controller = {
+	addNotes(name,value){
+		//Проверка на пустую строку
+		if (name.trim() !== '' && value.trim() !== '') {
+			//Передаем содержимое обоих инпутов в model
+			model.addNotes(name, value)
+		} else {
+			console.log('в контроллер ничего не передалось')
+		}
+	}
+}
+
+function init(){
+	view.init()
+}
+
+init()
